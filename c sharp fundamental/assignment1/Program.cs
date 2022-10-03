2d6eb16212f79b100bfeb3f06a956c3326eae758
vinh1332002 <47056351+vinh1332002@users.noreply.github.com>
2022-10-03 14:34:05 +0700

    Update Program.cs

--- c sharp fundamental/assignment1/Program.cs
+++ c sharp fundamental/assignment1/Program.cs
@@ -1,4 +1,4 @@
-﻿namespace Assignment1
+namespace Assignment1
 {
     class Program
     {
@@ -6,11 +6,11 @@
         {
             List<Member> liststudent = new List<Member>() //Create new list of student
             {
-                new Member(){fname = "Vinh", lname = "Nguyen", gender = "Male", date_of_birth = new DateTime(2002, 3, 13), phone_number = "0964083300", birth_place = "Ha Noi", is_graduated = false},
-                new Member(){fname = "Duc", lname = "Bui", gender = "Female", date_of_birth = new DateTime(2002, 9, 30), phone_number = "0985483300", birth_place = "ha noi", is_graduated = false},
-                new Member(){fname = "Ngai", lname = "Nguyen", gender = "Male", date_of_birth = new DateTime(1999, 5, 30), phone_number = "0875463300", birth_place = "Thai Binh", is_graduated = true},
-                new Member(){fname = "Chuoi", lname = "Le", gender = "Female", date_of_birth = new DateTime(2001, 4, 24), phone_number = "0185683001", birth_place = "Quang Ngai", is_graduated = true},
-                new Member(){fname = "Than", lname = "Dang", gender = "Female", date_of_birth = new DateTime(2000, 10, 10), phone_number = "0285483780", birth_place = "Thai Nguyen", is_graduated = true}
+                new Member(){fName = "Vinh", lName = "Nguyen", gender = "Male", dateOfBirth = new DateTime(2002, 3, 13), phoneNumber = "0964083300", birthPlace = "Ha Noi", isGraduated = false},
+                new Member(){fName = "Duc", lName = "Bui", gender = "Female", dateOfBirth = new DateTime(2002, 9, 30), phoneNumber = "0985483300", birthPlace = "ha noi", isGraduated = false},
+                new Member(){fName = "Ngai", lName = "Nguyen", gender = "Male", dateOfBirth = new DateTime(1999, 5, 30), phoneNumber = "0875463300", birthPlace = "Thai Binh", isGraduated = true},
+                new Member(){fName = "Chuoi", lName = "Le", gender = "Female", dateOfBirth = new DateTime(2001, 4, 24), phoneNumber = "0185683001", birthPlace = "Quang Ngai", isGraduated = true},
+                new Member(){fName = "Than", lName = "Dang", gender = "Female", dateOfBirth = new DateTime(2000, 10, 10), phoneNumber = "0285483780", birthPlace = "Thai Nguyen", isGraduated = true}
             };
             Function.ListMale(liststudent);
             Function.OldestMember(liststudent);
