52bd1bc8cb3955c7e0a640432ab177d5d737a29d
vinh1332002 <47056351+vinh1332002@users.noreply.github.com>
2022-10-03 17:38:01 +0700

    Update Program.cs

--- c sharp fundamental/assignment1/Program.cs
+++ c sharp fundamental/assignment1/Program.cs
@@ -12,11 +12,45 @@ namespace Assignment1
                 new Member(){fName = "Chuoi", lName = "Le", gender = "Female", dateOfBirth = new DateTime(2001, 4, 24), phoneNumber = "0185683001", birthPlace = "Quang Ngai", isGraduated = true},
                 new Member(){fName = "Than", lName = "Dang", gender = "Female", dateOfBirth = new DateTime(2000, 10, 10), phoneNumber = "0285483780", birthPlace = "Thai Nguyen", isGraduated = true}
             };
-            Function.ListMale(liststudent);
-            Function.OldestMember(liststudent);
-            Function.ListFullName(liststudent);
-            Function.ListDateOfBirth(liststudent);
-            Function.FirstBornHaNoi(liststudent);
+             Console.WriteLine("Choose a number from 1 to 5 to show the list you want: \n 1. Member that are male in the list: \n 2. Oldest Member in the list: \n 3. List of Member with fullname:  " +
+            "\n 4. List of Member with date of birth comparison: \n 5. The first person who was born in Ha Noi: \n 6. End the program (Press other numbers to end)."
+            );
+
+            int choice = 0;
+            do
+            {
+                choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        {
+                            Function.ListMale(liststudent);
+                            break;
+                        }
+                    case 2:
+                        {
+                            Function.OldestMember(liststudent);
+                            break;
+                        }
+                    case 3:
+                        {
+                            Function.ListFullName(liststudent);
+                            break;
+                        }
+                    case 4:
+                        {
+                            Function.ListDateOfBirth(liststudent);
+                            break;
+                        }
+                    case 5:
+                        {
+                            Function.FirstBornHaNoi(liststudent);
+                            break;
+                        }
+                }
+            } while (choice >= 1 && choice <= 5);
+
+            Console.ReadKey();
         }
     }
 }
