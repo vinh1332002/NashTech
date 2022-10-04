4e378c49cf9bcad78a1735a9632a76ab03969dd2
vinh1332002 <47056351+vinh1332002@users.noreply.github.com>
2022-10-04 11:49:17 +0700

    Update Program.cs

--- c sharp fundamental/assignment2/Program.cs
+++ c sharp fundamental/assignment2/Program.cs
@@ -1,21 +1,67 @@
-﻿namespace Assignment2
+namespace Assignment2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            List<Member> liststudent = new List<Member>() //Create new list of student
-            {
-                new Member(){fName = "Vinh", lName = "Nguyen", gender = "Male", dateOfBirth = new DateTime(2002, 3, 13), phoneNumber = "0964083300", birthPlace = "Ha Noi", isGraduated = false},
-                new Member(){fName = "Duc", lName = "Bui", gender = "Female", dateOfBirth = new DateTime(2002, 9, 30), phoneNumber = "0985483300", birthPlace = "ha noi", isGraduated = false},
-                new Member(){fName = "Ngai", lName = "Nguyen", gender = "Male", dateOfBirth = new DateTime(1999, 5, 30), phoneNumber = "0875463300", birthPlace = "Thai Binh", isGraduated = true},
-                new Member(){fName = "Chuoi", lName = "Le", gender = "Female", dateOfBirth = new DateTime(2001, 4, 24), phoneNumber = "0185683001", birthPlace = "Quang Ngai", isGraduated = true},
-                new Member(){fName = "Than", lName = "Dang", gender = "Female", dateOfBirth = new DateTime(2000, 10, 10), phoneNumber = "0285483780", birthPlace = "Thai Nguyen", isGraduated = true}
-            };
+            List<Member> listStudent =
+                new List<Member>()
+                {
+                    new Member()
+                    {
+                        FName = "Vinh",
+                        LName = "Nguyen",
+                        Gender = "Male",
+                        DateOfBirth = new DateTime(2002, 3, 13),
+                        PhoneNumber = "0964083300",
+                        BirthPlace = "Ha Noi",
+                        IsGraduated = false
+                    },
+                    new Member()
+                    {
+                        FName = "Duc",
+                        LName = "Bui",
+                        Gender = "Female",
+                        DateOfBirth = new DateTime(2002, 9, 30),
+                        PhoneNumber = "0985483300",
+                        BirthPlace = "ha noi",
+                        IsGraduated = false
+                    },
+                    new Member()
+                    {
+                        FName = "Ngai",
+                        LName = "Nguyen",
+                        Gender = "Male",
+                        DateOfBirth = new DateTime(1999, 5, 30),
+                        PhoneNumber = "0875463300",
+                        BirthPlace = "Thai Binh",
+                        IsGraduated = true
+                    },
+                    new Member()
+                    {
+                        FName = "Chuoi",
+                        LName = "Le",
+                        Gender = "Female",
+                        DateOfBirth = new DateTime(2001, 4, 24),
+                        PhoneNumber = "0185683001",
+                        BirthPlace = "Quang Ngai",
+                        IsGraduated = true
+                    },
+                    new Member()
+                    {
+                        FName = "Than",
+                        LName = "Dang",
+                        Gender = "Female",
+                        DateOfBirth = new DateTime(2000, 10, 10),
+                        PhoneNumber = "0285483780",
+                        BirthPlace = "Thai Nguyen",
+                        IsGraduated = true
+                    }
+                };
 
-            Console.WriteLine("Choose a number from 1 to 5 to show the list you want: \n 1. Member that are male in the list: \n 2. Oldest Member in the list: \n 3. List of Member with fullname:  " +
-            "\n 4. List of Member with date of birth comparison: \n 5. The first person who was born in Ha Noi: \n 6. End the program (Press other numbers to end)."
-            );
+            Console
+                .WriteLine("Choose a number from 1 to 5 to show the list you want: \n 1. Member that are male in the list: \n 2. Oldest Member in the list: \n 3. List of Member with fulLName:  " +
+                "\n 4. List of Member with date of birth comparison: \n 5. The first person who was born in Ha Noi: \n 6. End the program (Press other numbers to end).");
 
             int choice = 0;
             do
@@ -25,31 +71,32 @@
                 {
                     case 1:
                         {
-                            Function.ListMale(liststudent);
+                            Function.GetListMale (listStudent);
                             break;
                         }
                     case 2:
                         {
-                            Function.OldestMember(liststudent);
+                            Function.GetOldestMember (listStudent);
                             break;
                         }
                     case 3:
                         {
-                            Function.ListFullName(liststudent);
+                            Function.GetListFullName (listStudent);
                             break;
                         }
                     case 4:
                         {
-                            Function.ListDateOfBirth(liststudent);
+                            Function.GetListDateOfBirth (listStudent);
                             break;
                         }
                     case 5:
                         {
-                            Function.FirstBornHaNoi(liststudent);
+                            Function.GetFirstBornHaNoi (listStudent);
                             break;
                         }
                 }
-            } while (choice >= 1 && choice <= 5);
+            }
+            while (choice >= 1 && choice <= 5);
 
             Console.ReadKey();
         }
