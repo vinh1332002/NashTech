6b71319968ab35361fc798533af877f14712187a
vinh1332002 <47056351+vinh1332002@users.noreply.github.com>
2022-10-22 11:52:32 +0700

    Update Program.cs

--- EFCore/assignment2/Program.cs
+++ EFCore/assignment2/Program.cs
@@ -3,7 +3,6 @@ using Microsoft.EntityFrameworkCore;
 using assignment2.Repositories;
 using assignment2.Services;
 
-
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
@@ -12,19 +11,15 @@ builder.Services.AddSwaggerGen();
 
 // Add services to the container.
 
-
 builder.Services.AddDbContext<DataContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DBConnString")));
-
 
 builder.Services.AddTransient<ICategoryRepository, CategoryRepository>();
 builder.Services.AddTransient<ICategoryService, CategoryService>();
 builder.Services.AddTransient<IProductRepository, ProductRepository>();
 builder.Services.AddTransient<IProductService, ProductService>();
 
-
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-
 
 var app = builder.Build();
 
